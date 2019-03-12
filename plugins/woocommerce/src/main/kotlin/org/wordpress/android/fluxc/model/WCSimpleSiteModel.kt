07d7847f536efb971bff32f0e5435ce8d3303b28
Nick Bradbury <nick.bradbury@gmail.com>
2019-03-12 13:50:25 -0400

    When converting WCSimpleSiteModel to a SiteModel, set its origin and jetpack value

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/WCSimpleSiteModel.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/WCSimpleSiteModel.kt
@@ -1,7 +1,17 @@
 package org.wordpress.android.fluxc.model
 
+import org.wordpress.android.fluxc.model.SiteModel.ORIGIN_WPCOM_REST
+
 class WCSimpleSiteModel(
     val siteId: Long,
     val url: String,
     val name: String
-)
+) {
+    fun toSiteModel() = SiteModel().also { site ->
+        site.siteId = siteId
+        site.url = url
+        site.name = name
+        site.setIsJetpackConnected(true)
+        site.origin = ORIGIN_WPCOM_REST
+    }
+}
