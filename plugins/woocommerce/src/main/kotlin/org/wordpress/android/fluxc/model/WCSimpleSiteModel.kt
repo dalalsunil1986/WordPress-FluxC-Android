7da5d1b32b2d966c41c961b124d09708452db607
Nick Bradbury <nick.bradbury@gmail.com>
2019-03-12 09:27:36 -0400

    Added WCSimpleSiteModel conversion to/from SiteModel

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/WCSimpleSiteModel.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/WCSimpleSiteModel.kt
@@ -4,4 +4,14 @@ class WCSimpleSiteModel(
     val siteId: Long,
     val url: String,
     val name: String
-)
+) {
+    companion object {
+        fun fromSiteModel(site: SiteModel): WCSimpleSiteModel = WCSimpleSiteModel(site.siteId, site.url, site.name)
+    }
+
+    fun toSiteModel(): SiteModel = SiteModel().also { site ->
+        site.siteId = siteId
+        site.url = url
+        site.name = name
+    }
+}
